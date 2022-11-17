34e5842ef4e2ecadec7b0551848a064e60f0a9da
Valentin Abadie <valentin.abadie@gmail.com>
2022-11-17 18:07:17 +0100

    a finir

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+  # def index
+  #   @bookmarks = bookmark.all
+  # end
+
+  # def show
+  #   @bookmark = bookmark.find(params[:id])
+  # end
+
+  # def new
+  #   @bookmark = bookmark.new
+  # end
+
+  # def create
+  #   @bookmark = bookmark.new(bookmark_params)
+  #   if @bookmark.save
+  #     redirect_to bookmark_path(@bookmark)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  # private
+
+  # def bookmark_params
+  #   params.require(:bookmark).permit(:name, :address, :phone_number, :category)
+  # end
+end
